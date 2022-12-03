1933eaa9bb1ff65a476576237fb5c227dd310a75
Évelyne <charlesdelorean1@icloud.com>
2022-12-03 08:54:31 -0500

    feat: more isns

--- decompiler/decompile.swift
+++ decompiler/decompile.swift
@@ -18,12 +18,22 @@ func mov(_ raw: String) -> (String) {
     return dest + " = " + source
 }
 
-func bl(_ raw: String) -> String {
+func adr(_ raw: String) -> (String) {
+    let comp = raw.components(separatedBy: " ").dropFirst().joined().components(separatedBy: ",")
+    let dest = comp[0]
+    let source = comp[1]
+    if dest.hasPrefix("x") || dest.hasPrefix("w") {
+        regs.x[String(dest.dropFirst())] = source
+    }
+    return dest + " = " + source
+}
+
+func branch(_ raw: String) -> String {
     var comp = raw.components(separatedBy: " ").dropFirst().first
     if let _comp = comp, _comp.hasPrefix("_") {
         comp = String(_comp.dropFirst())
     }
-    return (comp ?? "UNKNOWN_FUNCTION") + "(" + regs.x.compactMap(\.value).map(String.init).joined(separator: ", ") + ")"
+    return (comp ?? "UNKNOWN_FUNCTION") + "(" + regs.x.compactMap(\.value).compactMap { if let num = $0 as? Int { return String(num) }; return String(describing: $0) }.joined(separator: ", ") + ")"
 }
 
 var conditionCodes: [String: Bool] = [:]
@@ -32,14 +42,19 @@ func cmp(_ raw: String) -> String {
     let comp = raw.components(separatedBy: " ").dropFirst().joined().components(separatedBy: ",")
     let dest = comp[0]
     let source = comp[1].dropFirst()
-    if let reg = regs.x[String(dest.dropFirst())] {
-        conditionCodes["EQ"] = String(reg ?? 0) == source
+    if let register = regs.x[String(dest.dropFirst())],
+       let reg = register as? Int,
+       String(reg) != source {
+        regs.x[String(dest.dropFirst())] = nil
+        conditionCodes["EQ"] = true
         #if DEBUG
         return "// stripped out dead code: " + raw
         #else
         return ""
         #endif
     } else {
+        regs.x[String(dest.dropFirst())] = nil
+        conditionCodes["EQ"] = false
         indentCount += 1
         return "if (" + dest + " == " + String(source) + ") {"
     }
@@ -50,11 +65,31 @@ func beq(_ raw: String) -> String {
     if let _comp = comp, _comp.hasPrefix("_") {
         comp = String(_comp.dropFirst())
     }
-    let ret = (0..<indentCount).map({ _ in "    " }).joined() +
-    (comp ?? "UNKNOWN_FUNCTION") +
+    let ret = (comp ?? "UNKNOWN_FUNCTION") +
     "(" +
-    regs.x.compactMap(\.value).map(String.init).joined(separator: ", ") +
+    regs.x.compactMap(\.value).compactMap { $0 as? Int }.map(String.init).joined(separator: ", ") +
     ")"
     regs.x = [:] // can't trust regs after function call
     return ret
+}
+
+func csel(_ raw: String) -> String {
+    let comp = raw.components(separatedBy: " ").dropFirst().joined().components(separatedBy: ",")
+    let dest = comp[0]
+    let one = comp[1]
+    let two = comp[2]
+    let code = comp[3]
+    regs.x[String(one.dropFirst())] = nil
+    regs.x[String(two.dropFirst())] = nil
+    regs.x[String(dest.dropFirst())] = ((conditionCodes[code] == true) ? two : one)
+    return "// csel"
+}
+
+extension String {
+    func indent() -> String {
+        if indentCount >= 0 {
+            return (0..<indentCount).map({ _ in "    " }).joined() + self
+        }
+        return self
+    }
 }
--- decompiler/main.swift
+++ decompiler/main.swift
@@ -8,12 +8,55 @@ b.eq _exit
 mov x1, x0
 cmp x1, #1
 b.eq _exit
+mov x0, #2
+b _drm
 """
 
-var out: [String] = .init()
+let fns = [
+    "_main":
+        """
+        bl _hello_world
+        bl _set_app_env
+        bl _call_app_binary
+        bl _goodbye
+        ret
+        """,
+
+    "_goodbye":
+        """
+        adr x0, goodbye
+        bl _puts
+        ret
+        """,
+    "_hello_world":"""
+        adr x0, hello_world
+        bl _puts
+        ret
+        """,
+    "_set_app_env":
+        """
+        adr x0, dyld_insert
+        adr x1, dylib_path
+        mov x2, #1
+        bl _setenv
+        cmp x0, #0
+        adr x1, success
+        adr x2, failure
+        csel x0, x1, x2, EQ
+        bl _puts
+        ret
+        """,
+    "_call_app_binary":
+        """
+        adr x0, bin_path
+        adr x1, bin_path
+        bl _execl
+        ret
+        """,
+]
 
 struct Regs {
-    var x: [String:Int?]
+    var x: [String:Any?]
     var sp: Int
 }
 
@@ -21,46 +64,61 @@ var regs: Regs = .init(x: [:], sp: 0)
 
 var indentCount: Int = 0
 
-for line in str.components(separatedBy: "\n") {
-    let opc = line.components(separatedBy: " ").first
-    switch opc {
-    case "mov":
-        out.append(mov(line))
-    case "bl":
-        out.append(bl(line))
-    case "cmp":
-        out.append(cmp(line))
-    case "b.eq":
-        if let code = conditionCodes["EQ"] {
-            if code {
-                out.append(beq(line))
+fns.forEach { name, fn in
+    
+    regs = .init(x: [:], sp: 0)
+    indentCount = 0
+    
+    var out: [String] = .init()
+
+    let isns = fn.components(separatedBy: "\n")
+
+    for (idx, line) in isns.enumerated() {
+        let opc = line.components(separatedBy: " ").first
+        switch opc {
+        case "mov":
+            out.append(mov(line).indent())
+        case "adr":
+            out.append(adr(line).indent())
+        case "bl":
+            out.append(branch(line).indent())
+        case "cmp":
+            out.append(cmp(line))
+        case "csel":
+            out.append(csel(line).indent())
+        case "b.eq":
+            if let code = conditionCodes["EQ"] {
+                if code {
+                    out.append(beq(line).indent())
+                } else {
+                    #if DEBUG
+                    out.append("// stripped out dead code: " + line)
+                    #endif
+                }
             } else {
-                #if DEBUG
-                out.append("// stripped out dead code: " + line)
-                #endif
-                conditionCodes.removeValue(forKey: "EQ")
+                out.append(beq(line).indent())
             }
-        } else {
-            out.append(beq(line))
+            conditionCodes.removeValue(forKey: "EQ")
+            indentCount -= 1
+            out.append("}")
+        case "b":
+            out.append("return " + branch(line).indent())
+        case "ret":
+            out.append("return".indent())
+        default: out.append((#"__asm__(""# + line + #"")"#).indent())
         }
-    default: continue
     }
-}
-
-print("---------- DISASM -----------")
-
-print(str)
-
-print("---------- DECOMP -----------")
-
-print(out.joined(separator: "\n"))
-
-if indentCount != 0 {
-    for i in 1...indentCount {
-        print((0..<(indentCount - i)).map { _ in "  "}.joined() + "}")
+    
+    print("\n")
+    print("void " + name.dropFirst() + "(void)" + " {")
+    
+    print(out.compactMap { "    " + $0 } .joined(separator: "\n"))
+    
+    if indentCount >= 1 {
+        for i in 1...indentCount {
+            print((0..<(indentCount - i)).map { _ in "  "}.joined() + "    }")
+        }
     }
+    
+    print("}")
 }
-
-print("---------- REGISTERS -----------")
-
-print(regs)
